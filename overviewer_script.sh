e95db53dab7c0cf7a449566acc2e86d9acedf9e9
mikesull20 <mikesull20@gmail.com>
2022-11-05 19:27:03 -0400

    Latest Commit

--- overviewer_script.sh
+++ overviewer_script.sh
@@ -1,5 +1,5 @@
 #bash
-#docker run --rm -v /var/lib/docker/volumes/minecraftdata/_data/:/tmp/server/:ro -v /var/lib/docker/volumes/minecraftdata/_data/scripts/:/tmp/config/:ro -v /var/lib/docker/volumes/minecraftdata/_data/Overviewer/:/tmp/export/:rw -it marctv/minecraft-overviewer
+docker run --rm -v /var/lib/docker/volumes/minecraftdata/_data/:/tmp/server/:ro -v /var/lib/docker/volumes/minecraftdata/_data/scripts/:/tmp/config/:ro -v /var/lib/docker/volumes/minecraftdata/_data/Overviewer/:/tmp/export/:rw -it marctv/minecraft-overviewer
 git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ init
 git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ reset
 git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ add /var/lib/docker/volumes/minecraftdata/_data/Overviewer/*
