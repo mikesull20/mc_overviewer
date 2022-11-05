1de6af1ce4ef9eeb56f267d99f5098309f45e6c7
mikesull20 <mikesull20@gmail.com>
2022-11-05 19:18:41 -0400

    Latest Commit

--- overviewer_script.sh
+++ overviewer_script.sh
@@ -1,9 +1,9 @@
 #bash
-docker run --rm -v /var/lib/docker/volumes/minecraftdata/_data/:/tmp/server/:ro -v /var/lib/docker/volumes/minecraftdata/_data/scripts/:/tmp/config/:ro -v /var/lib/docker/volumes/minecraftdata/_data/Overviewer/:/tmp/export/:rw -it marctv/minecraft-overviewer
-git init
-git reset
-git add /var/lib/docker/volumes/minecraftdata/_data/Overviewer/*
-git commit -m "Latest Commit"
-git branch -M main
-git remote add origin git@github.com:mikesull20/mc_overviewer.git
-git push -u origin main
+#docker run --rm -v /var/lib/docker/volumes/minecraftdata/_data/:/tmp/server/:ro -v /var/lib/docker/volumes/minecraftdata/_data/scripts/:/tmp/config/:ro -v /var/lib/docker/volumes/minecraftdata/_data/Overviewer/:/tmp/export/:rw -it marctv/minecraft-overviewer
+git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ init
+git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ reset
+git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ add /var/lib/docker/volumes/minecraftdata/_data/Overviewer/*
+git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ commit -m "Latest Commit"
+git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ branch -M main
+git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ remote add origin git@github.com:mikesull20/mc_overviewer.git
+git -C /var/lib/docker/volumes/minecraftdata/_data/Overviewer/ push -u origin main
