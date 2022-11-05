a43e0668c7e8545bcff8f010c048a5626e8e2c60
mikesull20 <mikesull20@gmail.com>
2022-11-05 15:52:06 -0400

    Latest Commit

--- overviewer_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#docker run --rm -v /var/lib/docker/volumes/minecraftdata/_data/:/tmp/server/:ro -v /var/lib/docker/volumes/minecraftdata/_data/scripts/:/tmp/config/:ro -v /var/lib/docker/volumes/minecraftdata/_data/Overviewer/:/tmp/export/:rw -it marctv/minecraft-overviewer
-git init
-git add /var/lib/docker/volumes/minecraftdata/_data/Overviewer/*
-git commit -m "Latest Commit"
-git branch -M main
-git remote add origin git@github.com:mikesull20/mc_overviewer.git
-git push -u origin main
